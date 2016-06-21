4ba92fc022d2494a8cf6dff0d5d9c7baff912deb
李京生 <307771861@qq.com>
2016-06-21 13:18:08 +0800

    更新代码

---
+++ app/view1/view1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute','ngCookies'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope','$cookieStore',function($scope,$cookieStore) {
+  $cookieStore.put("name","my name");
+  $cookieStore.get("name") == "my name";
+  //$cookieStore.get("__ngDebug");
+
+  $cookieStore.put("person", {
+    name: "my name",
+    age: 18
+  });
+
+  $scope.person = $cookieStore.get("lc");
+  console.log($scope.person);
+}]);
